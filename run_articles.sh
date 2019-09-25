666f03a85396491a54919aa204e94b609429850f
vladiscripts <blagopoluchie12@gmail.com>
2019-09-25 20:28:18 +0300

    is_item_of_disambig, set_or_remove_category()

--- run_articles.sh
+++ run_articles.sh
@@ -4,4 +4,4 @@
 source $PYTHONENV/bin/activate
 cd ~/cgi-bin/ws_interlinker_via_wikidata
 # PYTHONIOENCODING="utf-8"
-LANG="ru_RU.UTF-8" ./articles.py -titleregexnot:"^ТСД" -cat:"Ручная ссылка:Википедия|И" -onlyif:P31=Q13433827
+LANG="ru_RU.UTF-8" ./articles.py -titleregexnot:"^ТСД" -cat:"Ручная ссылка:Википедия" -onlyif:P31=Q13433827
