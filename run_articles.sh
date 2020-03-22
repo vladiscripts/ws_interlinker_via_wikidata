dad8f1bfc4f9f45b042681b73b0e646f3c5fc5de
vladiscripts <blagopoluchie12@gmail.com>
2020-03-22 21:09:04 +0300

    separated get_item() for clear signatures

--- run_articles.sh
+++ run_articles.sh
@@ -2,7 +2,7 @@
 # jsub -once -e ~/cgi-bin/patrolpage/temp/ -o ~/cgi-bin/patrolpage/temp/ -N patrolpage ./run.sh
 # jsub -once -N articles ./run_articles.sh
 source $PYTHONENV/bin/activate
-cd ~/cgi-bin/ws_interlinker_via_wikidata
+cd ~/scripts/ws_interlinker_via_wikidata
 # PYTHONIOENCODING="utf-8"
 LANG="ru_RU.UTF-8" ./articles.py -titleregexnot:"^ТСД" -cat:"Ручная ссылка:Википедия" -onlyif:P31=Q13433827
 # LANG="ru_RU.UTF-8" ./articles.py -titleregexnot:"^ТСД" -cat:"Ручная ссылка:Википедия" -onlyif:P31=Q17329259
