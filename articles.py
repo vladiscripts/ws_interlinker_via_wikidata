573a9701411287e2360c405b92f4467e9b0e6d82
vladiscripts <nSm5wxm4>
2021-03-29 01:07:51 +0300

    * disambigs() moved

--- articles.py
+++ articles.py
@@ -115,18 +115,6 @@ class Articles(Process):
                     else:
                         wiki_util.remove_param(p, param.name)
 
-    def disambigs(self, p, param):
-        """ссылки на дизамбиги"""
-        # is_item_of_disambig = self.wd.is_item_of_disambig(param.item)
-        wiki_util.set_or_remove_category(p, cat_name=f'Ручная ссылка на неоднозначность:{param.name.capitalize()}',
-                                         condition=param.is_item_of_disambig, add_cat=self.skip_wd_links_to_disambigs,
-                                         # log_on_add=f'{param.name}: ссылка на дизамбиг'
-                                         )
-        # if param.is_item_of_disambig and self.skip_wd_links_to_disambigs:
-        #     pwb.stdout(f'{param.name}: ссылка на дизамбиг')
-        #     p.do_skip = True
-        #     return True
-
 
 if __name__ == '__main__':
     # as_bot = True
