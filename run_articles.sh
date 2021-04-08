83b2445af79d648e3f2d9f8ebc21ce657393a1a5
vladiscripts <nSm5wxm4>
2021-04-08 15:49:01 +0300

    *path

--- run_articles.sh
+++ run_articles.sh
@@ -2,7 +2,7 @@
 # jsub -once -e ~/cgi-bin/patrolpage/temp/ -o ~/cgi-bin/patrolpage/temp/ -N patrolpage ./run.sh
 # jsub -once -N articles ./run_articles.sh
 source $PYTHONENV/bin/activate
-cd ~/scripts/ws_interlinker_by_wikidata
+cd ~/scripts/interlinker_by_wikidata_ws
 # PYTHONIOENCODING="utf-8"
 LANG="ru_RU.UTF-8" ./articles.py
 # LANG="ru_RU.UTF-8" ./articles.py -titleregexnot:"^ТСД" -cat:"Ручная ссылка:Википедия" -onlyif:P31=Q13433827
